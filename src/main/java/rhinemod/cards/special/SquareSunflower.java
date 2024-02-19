b8802b7cd516767ac22d37f6cf2ce4a7d1bdd221
noname512 <zxononame@gmail.com>
2024-02-18 20:28:52 -0500

    方形葵描述修正

--- src/main/java/rhinemod/cards/special/SquareSunflower.java
+++ src/main/java/rhinemod/cards/special/SquareSunflower.java
@@ -5,7 +5,6 @@ import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import rhinemod.patches.RhineTags;
