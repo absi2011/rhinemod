05d57927e402adc1dd642a1977ea73a8818ba246
noname512 <zxononame@gmail.com>
2024-03-09 22:13:22 -0500

    四层精英50%概率为唤醒

---
+++ src/main/java/rhinemod/patches/EndingElitePatch.java
@@ -0,0 +1,27 @@
+package rhinemod.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.dungeons.TheEnding;
+
+import java.util.logging.Logger;
+
+public class EndingElitePatch {
+    @SpirePatch(clz = TheEnding.class, method = "generateMonsters")
+    public static class GenerateMonstersPatch {
+        @SpirePostfixPatch
+        public static void Postfix(TheEnding _inst) {
+            if (AbstractDungeon.mapRng.random(0, 1) == 1) {
+                TheEnding.monsterList.clear();
+                TheEnding.eliteMonsterList.clear();
+                TheEnding.monsterList.add("Awaken");
+                TheEnding.monsterList.add("Awaken");
+                TheEnding.monsterList.add("Awaken");
+                TheEnding.eliteMonsterList.add("Awaken");
+                TheEnding.eliteMonsterList.add("Awaken");
+                TheEnding.eliteMonsterList.add("Awaken");
+            }
+        }
+    }
+}
