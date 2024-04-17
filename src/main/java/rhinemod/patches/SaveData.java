7aeea3ffd50f58cc97811d80e1f9be0cb856158e
noname512 <zxononame@gmail.com>
2024-04-17 00:12:48 -0400

    SL保留卡牌升级分支

--- src/main/java/rhinemod/patches/SaveData.java
+++ src/main/java/rhinemod/patches/SaveData.java
@@ -3,21 +3,66 @@ package rhinemod.patches;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.cards.CardSave;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.saveAndContinue.SaveAndContinue;
 import com.megacrit.cardcrawl.saveAndContinue.SaveFile;
+import com.megacrit.cardcrawl.screens.select.GridCardSelectScreen;
 import javassist.CtBehavior;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import rhinemod.cards.AbstractRhineCard;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SaveData {
+
+    @SpirePatch(clz = CardSave.class, method = SpirePatch.CLASS)
+    public static class OptFields {
+        public static SpireField<Integer> branch = new SpireField<>(() -> 0);
+    }
     private static final Logger logger = LogManager.getLogger(SaveData.class);
+
+    @SpirePatch(clz = CardGroup.class, method = "getCardDeck")
+    public static class GetCardDeckPatch {
+        @SpirePrefixPatch
+        public static SpireReturn<?> Prefix(CardGroup _inst) {
+            ArrayList<CardSave> retVal = new ArrayList<>();
+            for (AbstractCard card : _inst.group) {
+                CardSave cs = new CardSave(card.cardID, card.timesUpgraded, card.misc);
+                if (card instanceof AbstractRhineCard)
+                    OptFields.branch.set(cs, ((AbstractRhineCard) card).chosenBranch);
+                retVal.add(cs);
+            }
+            return SpireReturn.Return(retVal);
+        }
+    }
+
+    @SpirePatch(clz = CardCrawlGame.class, method = "loadPlayerSave")
+    public static class LoadPlayerSavePatch {
+        @SpirePostfixPatch
+        public static void Postfix(CardCrawlGame _inst, AbstractPlayer p, SaveFile ___saveFile) {
+            p.masterDeck.clear();
+            for (CardSave s : ___saveFile.cards) {
+                AbstractCard c = CardLibrary.getCopy(s.id, s.upgrades, s.misc);
+                if (c instanceof AbstractRhineCard && OptFields.branch.get(s) > 0) {
+                    ((AbstractRhineCard) c).swapBranch(OptFields.branch.get(s));
+                }
+                p.masterDeck.addToTop(c);
+            }
+        }
+    }
+
     @SpirePatch(clz = SaveFile.class, method = "<ctor>", paramtypez = {SaveFile.SaveType.class})
     public static class SaveTheSaveData {
         @SpirePostfixPatch
-        public static void Postfix(SaveFile __instance, SaveFile.SaveType type) {
+        public static void Postfix(SaveFile _inst, SaveFile.SaveType type) {
             SaveData.logger.info("Extra Data Saved!");
         }
     }
@@ -60,7 +105,7 @@ public class SaveData {
     @SpirePatch(clz = AbstractDungeon.class, method = "loadSave")
     public static class loadSavePatch {
         @SpirePostfixPatch
-        public static void Postfix(AbstractDungeon __instance, SaveFile file) {
+        public static void Postfix(AbstractDungeon _inst, SaveFile file) {
             SaveData.logger.info("Save loaded.");
         }
     }
