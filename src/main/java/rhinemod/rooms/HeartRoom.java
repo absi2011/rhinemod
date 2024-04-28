ce9f7b7ad3205d6edd5e2c4175bf2f085b92494d
absi2011 <absi2011@126.com>
2024-04-29 01:55:27 +0800

    新增命运所指Room，避免心脏变商店或宝箱

--- src/main/java/rhinemod/rooms/HeartRoom.java
+++ src/main/java/rhinemod/rooms/HeartRoom.java
@@ -1,14 +1,28 @@
 package rhinemod.rooms;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.events.exordium.Mushrooms;
+import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.map.Legend;
+import com.megacrit.cardcrawl.map.LegendItem;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.rooms.EventRoom;
+import com.megacrit.cardcrawl.screens.CombatRewardScreen;
+import com.megacrit.cardcrawl.ui.buttons.ProceedButton;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import javassist.CannotCompileException;
+import javassist.NotFoundException;
+import javassist.expr.ExprEditor;
+import javassist.expr.Instanceof;
 import rhinemod.events.HeartEvent;
 
 public class HeartRoom extends AbstractRoom{
-    public com.megacrit.cardcrawl.rooms.VictoryRoom.EventType eType;
-
     public HeartRoom() {
         this.phase = RoomPhase.EVENT;
         this.mapSymbol = "?";
@@ -28,6 +42,10 @@ public class HeartRoom extends AbstractRoom{
             this.event.update();
         }
 
+        if (this.event.waitTimer == 0.0F && !this.event.hasFocus && this.phase != RoomPhase.COMBAT) {
+            this.phase = RoomPhase.COMPLETE;
+            this.event.reopen();
+        }
     }
 
     public void render(SpriteBatch sb) {
@@ -45,4 +63,22 @@ public class HeartRoom extends AbstractRoom{
             this.event.renderAboveTopPanel(sb);
         }
     }
+
+    public void addPotionToRewards() {
+    }
+
+    /* Copied from ArenaRoom */
+    // 没看懂，抄一遍
+    @SpirePatch(clz = AbstractDungeon.class, method = "render")
+    public static class RenderPatch {
+        @SpireInsertPatch(rloc = 36)
+        public static void Insert(AbstractDungeon __instance, SpriteBatch sb) {
+            if (AbstractDungeon.getCurrRoom() instanceof HeartRoom) {
+                AbstractDungeon.getCurrRoom().renderEventTexts(sb);
+            }
+        }
+    }
+
 }
+
+
