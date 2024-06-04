583134cca46721104b2a6997e162dd62db4c824a
noname512 <zxononame@gmail.com>
2024-06-04 11:07:57 -0400

    新增另外一些卡图

--- src/main/java/rhinemod/cards/LightPenetratingClouds.java
+++ src/main/java/rhinemod/cards/LightPenetratingClouds.java
@@ -1,33 +1,29 @@
 package rhinemod.cards;
 
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import rhinemod.cards.special.Unscrupulous;
 import rhinemod.interfaces.UpgradeBranch;
 import rhinemod.patches.AbstractCardEnum;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ResearchCapital extends AbstractRhineCard {
-    public static final String ID = "rhinemod:ResearchCapital";
+public class LightPenetratingClouds extends AbstractRhineCard {
+    public static final String ID = "rhinemod:LightPenetratingClouds";
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
-    public static final String IMG = "resources/rhinemod/images/cards/SHAFT.png";
+    public static final String IMG = "resources/rhinemod/images/cards/LightPenetratingClouds.png";
     public static final int COST = 1;
     public static final int BASIC_DAMAGE = 8;
     public static final int UPGRADE_DAMAGE = 2;
-    public ResearchCapital() {
+    public LightPenetratingClouds() {
         super(ID, NAME, IMG, COST, DESCRIPTION,
                 CardType.ATTACK, AbstractCardEnum.RHINE_MATTE,
                 CardRarity.UNCOMMON, CardTarget.ENEMY);
