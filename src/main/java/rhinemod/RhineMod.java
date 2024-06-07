0c550d07be8abe865d79df4fc95a695a57508353
absi2011 <absi2011@126.com>
2024-06-07 23:38:58 +0800

    削弱机械之灾

--- src/main/java/rhinemod/RhineMod.java
+++ src/main/java/rhinemod/RhineMod.java
@@ -126,7 +126,29 @@ public class RhineMod implements EditCardsSubscriber, EditCharactersSubscriber, 
         BaseMod.addStrongMonsterEncounter(TheBeyond.ID, new MonsterInfo("ArcTeam", NewMonsterMulti));
         addMonster("Jesselton", names[10],  () -> new MonsterGroup(new AbstractMonster[] {new JesseltonWilliams(0.0F, 0.0F)}));
         BaseMod.addStrongMonsterEncounter(TheBeyond.ID, new MonsterInfo("Jesselton", NewMonsterMulti));
-        addMonster("Disaster of Machine", names[11], () -> new MonsterGroup(new AbstractMonster[] {new Perpetrator(-440.0F, 0.0F),new Exploder(-300.0F, 400.0F),  new Repulsor(-120.0F, 360.0F),  new Sentry(0.0F, 0.0F), new BronzeOrb(200.0F, 270.0F, 0), new Spiker(240.0F, 0.0F), new Crossroads(-220.0F, -50.0F)}));
+        addMonster("Disaster of Machine", names[11], () -> new MonsterGroup(new AbstractMonster[] {
+                new Perpetrator(-440.0F, 0.0F) {
+                    {setHp(maxHealth / 2);}
+                },
+                new Exploder(-300.0F, 400.0F),
+                new Repulsor(-120.0F, 360.0F),
+                new Sentry(0.0F, 0.0F) {
+                    {setHp(maxHealth - 10);}
+                    @Override
+                    protected void getMove(int num) {
+                        if (this.lastMove((byte)3)) {
+                            this.setMove((byte)4, Intent.ATTACK, this.damage.get(0).base);
+                        } else {
+                            this.setMove((byte)3, Intent.DEBUFF);
+                        }
+                    }
+                },
+                new BronzeOrb(200.0F, 270.0F, 0) {
+                    {setHp(maxHealth - 10);}
+                },
+                new Spiker(240.0F, 0.0F),
+                new Crossroads(-220.0F, -50.0F)
+        }));
         BaseMod.addEliteEncounter(TheBeyond.ID, new MonsterInfo("Disaster of Machine", 2.0F * NewMonsterMulti));
 
         // Add a name.
