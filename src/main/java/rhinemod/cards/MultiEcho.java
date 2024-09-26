f6b2b2d64a7c844cee45913db5661473458d5db9
absi2011 <absi2011@126.com>
2024-09-27 00:00:05 +0800

    多重分身相关卡牌

--- src/main/java/rhinemod/cards/MultiEcho.java
+++ src/main/java/rhinemod/cards/MultiEcho.java
@@ -2,12 +2,14 @@ package rhinemod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import rhinemod.actions.AddFlowingShapeAction;
+import rhinemod.actions.MultiEchoAction;
 import rhinemod.characters.RhineLab;
 import rhinemod.interfaces.UpgradeBranch;
 import rhinemod.patches.AbstractCardEnum;
@@ -15,33 +17,25 @@ import rhinemod.patches.AbstractCardEnum;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FlowCombo extends AbstractRhineCard {
-    public static final String ID = "rhinemod:FlowCombo";
+public class MultiEcho extends AbstractRhineCard {
+    public static final String ID = "rhinemod:MultiEcho";
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     public static final String IMG = "resources/rhinemod/images/cards/BionicDevice.png";
     public static final int COST = 1;
-    public static final int DAMAGE = 6;
-    public static final int UPGRADE_DAMAGE = 2;
-    public FlowCombo() {
+    public static final int UPGRADE_COST = 0;
+    public MultiEcho() {
         super(ID, NAME, IMG, COST, DESCRIPTION,
-                CardType.ATTACK, AbstractCardEnum.RHINE_MATTE,
-                CardRarity.UNCOMMON, CardTarget.ENEMY);
-        damage = baseDamage = DAMAGE;
+                CardType.SKILL, AbstractCardEnum.RHINE_MATTE,
+                CardRarity.RARE, CardTarget.SELF);
         realBranch = 3;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if (p instanceof RhineLab) {
-            int cnt = ((RhineLab) p).globalAttributes.flowspNum;
-            addToBot(new AddFlowingShapeAction(-cnt));
-            for (int i = 0; i < cnt; i++) {
-                addToBot(new DamageAction(m, new DamageInfo(p, damage), AbstractGameAction.AttackEffect.POISON));
-            }
-        }
+        addToBot(new MultiEchoAction());
     }
 
     @Override
@@ -50,7 +44,7 @@ public class FlowCombo extends AbstractRhineCard {
             add(() -> {
                 if (!upgraded) {
                     upgradeName(0);
-                    upgradeDamage(UPGRADE_DAMAGE);
+                    upgradeBaseCost(UPGRADE_COST);
                     initializeDescription();
                 }
             });
