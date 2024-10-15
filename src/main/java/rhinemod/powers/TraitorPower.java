32dd50e5fdce44f7daf74bbc0e2e933ea0922407
noname512 <zxononame@gmail.com>
2024-10-15 00:58:18 -0400

    背叛者改为在onAttackedToChangeDamage后生效

--- src/main/java/rhinemod/powers/TraitorPower.java
+++ src/main/java/rhinemod/powers/TraitorPower.java
@@ -1,12 +1,17 @@
 package rhinemod.powers;
 
+import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import javassist.CtBehavior;
 import rhinemod.characters.RhineLab;
 import rhinemod.util.GlobalAttributes;
+
+import java.util.logging.Logger;
 
 public class TraitorPower extends AbstractDescriptionPower {
     public static final String POWER_ID = "rhinemod:TraitorPower";
@@ -27,14 +32,26 @@ public class TraitorPower extends AbstractDescriptionPower {
         description = DESCRIPTIONS[0];
     }
 
-    @Override
-    public int onAttackToChangeDamage(DamageInfo info, int damageAmount) {
-        // TODO: patch 在 onAttackedToChangeDamage 之后
-        if (AbstractDungeon.player instanceof RhineLab && info.type == DamageInfo.DamageType.NORMAL && damageAmount >= GlobalAttributes.smashThreshold) {
-            flashWithoutSound();
-            return damageAmount * 2;
-        } else {
-            return damageAmount;
+    @SpirePatch(clz = AbstractMonster.class, method = "damage")
+    public static class ChangeDamagePatch {
+        @SpireInsertPatch(locator = Locator.class, localvars = {"damageAmount"})
+        public static void Insert(AbstractMonster _inst, DamageInfo info, @ByRef int[] damageAmount) {
+            if (info.owner == AbstractDungeon.player && AbstractDungeon.player.hasPower(TraitorPower.POWER_ID)) {
+                if (AbstractDungeon.player instanceof RhineLab && info.type == DamageInfo.DamageType.NORMAL && damageAmount[0] >= GlobalAttributes.smashThreshold) {
+                    AbstractDungeon.player.getPower(TraitorPower.POWER_ID).flashWithoutSound();
+                    damageAmount[0] *= 2;
+                }
+            }
+        }
+
+        private static class Locator extends SpireInsertLocator {
+            @Override
+            public int[] Locate(CtBehavior ctBehavior) throws Exception {
+                Matcher.FieldAccessMatcher fieldAccessMatcher = new Matcher.FieldAccessMatcher(DamageInfo.class, "owner");
+                int[] totalTimes = LineFinder.findAllInOrder(ctBehavior, fieldAccessMatcher);
+                Logger.getLogger(TraitorPower.class.getName()).info("find " + totalTimes.length + " \"owners\"");
+                return new int[] {totalTimes[3]};
+            }
         }
     }
 }
