3aa81a2a1e5ae786e933f2dced9a2e2afd8ed744
noname512 <zxononame@gmail.com>
2024-10-19 18:14:05 -0400

    潘多拉魔盒特判

--- src/main/java/rhinemod/patches/StarterRelicPatch.java
+++ src/main/java/rhinemod/patches/StarterRelicPatch.java
@@ -1,29 +1,38 @@
 package rhinemod.patches;
 
-import com.badlogic.gdx.math.MathUtils;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
-import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.vfx.UpgradeShineEffect;
-import com.megacrit.cardcrawl.vfx.cardManip.ShowCardBrieflyEffect;
+import com.megacrit.cardcrawl.relics.PandorasBox;
 import rhinemod.relics.RhineLabEmployeeCard;
 import rhinemod.relics.TITStudentIdCard;
-
-import java.util.ArrayList;
 
 public class StarterRelicPatch {
     @SpirePatch(clz = CardGroup.class, method = "removeCard", paramtypez = {AbstractCard.class})
     public static class OnRemoveCardFromMasterDeckPatch {
         @SpirePostfixPatch
         public static void Post(CardGroup _inst, AbstractCard c) {
-            if (_inst.type == CardGroup.CardGroupType.MASTER_DECK && AbstractDungeon.player.hasRelic(TITStudentIdCard.ID)) {
-                AbstractDungeon.player.getRelic(TITStudentIdCard.ID).counter ++;
+            if (_inst.type == CardGroup.CardGroupType.MASTER_DECK) {
+                triggerStarterRelic(1);
             }
-            if (_inst.type == CardGroup.CardGroupType.MASTER_DECK && AbstractDungeon.player.hasRelic(RhineLabEmployeeCard.ID)) {
-                AbstractDungeon.player.getRelic(RhineLabEmployeeCard.ID).counter ++;
-            }
+        }
+    }
+
+    @SpirePatch(clz = PandorasBox.class, method = "onEquip")
+    public static class PandorasBoxPatch {
+        @SpirePostfixPatch
+        public static void Postfix(PandorasBox _inst, int ___count) {
+            triggerStarterRelic(___count);
+        }
+    }
+
+    public static void triggerStarterRelic(int cnt) {
+        if (AbstractDungeon.player.hasRelic(TITStudentIdCard.ID)) {
+            AbstractDungeon.player.getRelic(TITStudentIdCard.ID).counter += cnt;
+        }
+        if (AbstractDungeon.player.hasRelic(RhineLabEmployeeCard.ID)) {
+            AbstractDungeon.player.getRelic(RhineLabEmployeeCard.ID).counter += cnt;
         }
     }
 }
