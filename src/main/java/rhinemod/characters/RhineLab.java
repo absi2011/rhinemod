4ae481cb8690c6a39cb514bbf0ce8d44fc2c35a3
noname512 <zxononame@gmail.com>
2025-01-11 01:38:28 +0800

    default防炸

--- src/main/java/rhinemod/characters/RhineLab.java
+++ src/main/java/rhinemod/characters/RhineLab.java
@@ -226,7 +226,7 @@ public class RhineLab extends CustomPlayer {
 
     @Override
     public void preBattlePrep() {
-        if (!TipTracker.tips.get("RHINE_COMBAT_TIP")) {
+        if (!TipTracker.tips.getOrDefault("RHINE_COMBAT_TIP", false)) {
             AbstractDungeon.ftue = new RhineTipsFtue();
             TipTracker.neverShowAgain("RHINE_COMBAT_TIP");
         }
